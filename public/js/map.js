c7bd6ad6c45920ddb905b0737727f096f5a790a9
Vadasz Laszlo <maxinteger@gmail.com>
2014-05-02 13:48:14 +0200

    rewrite public side in angularjs

--- public/js/map.js
+++ public/js/map.js
@@ -2,210 +2,86 @@
  * Created by vadasz on 2014.04.25..
  */
 
-    //+ Jonas Raoni Soares Silva
-    //@ http://jsfromhell.com/math/is-point-in-poly [rev. #0]
+var App = angular.module('Ifiszallasok', []);
 
-function isPointInPoly(poly, pt){
-    for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
-        ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
-        && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
-        && (c = !c);
-    return c;
-}
+App.directive('map', ['CountyService', function(CountyService){
+    return {
+        link: function(scope, element, attrs){
+            var mapCood = new google.maps.LatLng(47.0,19.0),
+                mapOptions = {
+                    zoom: 7,
+                    center: mapCood,
+                    panControl: true,
+                    zoomControl: true,
+                    scaleControl: true,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
 
-var KML_URL = "http://pipes.yahoo.com/pipes/pipe.run?_id=54b2cac8daccd0be70a9516f6fce5d61&_render=json&_callback=loadKML",
-    infoWin = new google.maps.InfoWindow({maxWidth: 400}),
-    map = null;
-
-function loadScript(filename){
-    var fileref=document.createElement('script')
-    fileref.setAttribute("type","text/javascript")
-    fileref.setAttribute("src", filename)
-    document.getElementsByTagName("head")[0].appendChild(fileref)
-}
-
-function initializeMap() {
-    var mapCood = new google.maps.LatLng(47.0,19.0);
-    var mapOptions = {
-        zoom: 7,
-        center: mapCood,
-        panControl: true,
-        zoomControl: true,
-        scaleControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    /*
-     var kmlLayer = new google.maps.KmlLayer({
-     url: KML_URL,
-     suppressInfoWindows: true,
-     map: map
-     });
-     */
-    return map;
-}
-
-var loadKML = function( data){
-    $mlist = $("#marker-list")
-    $mlist.on('click', 'li.location', function(e){
-        if (!$(e.target).is('.zoom')){
-            var marker = $(this).data('marker');
-            google.maps.event.trigger(marker, 'click')
-        }
-    }).on('click', 'a.zoom', function (e) {
-        e.preventDefault()
-        $(this).closest('.location').click()
-        map.setZoom(15);
-    }).on ('mouseover', 'li.location', function(){
-        var marker = $(this).data('marker'),
-            icon = marker.getIcon();
-        marker.setAnimation(google.maps.Animation.DROP)
-        marker.setIcon(null)
-        $(this).one('mouseout', function(){
-            if(map.getZoom() > 10){
-                marker.setAnimation(null)
-            }
-            marker.setIcon(icon)
-        })
-    })
+                map = new google.maps.Map(element[0], mapOptions),
+                infoWin = new google.maps.InfoWindow({maxWidth: 400});
 
 
-    function addEvent(marker, desc){
-        google.maps.event.addListener(marker, 'click', function(e) {
-            infoWin.setContent(desc)
-            infoWin.open(map, marker)
-            //map.setZoom(15);
-            map.panTo(marker.position);
-        });
-    }
-
-    data = data.value.items[0].Document
-
-    var boundList = [], styles = {}
-
-    $.each(data.Style, function(){
-        var id = '#' + this.id;
-        if (this.LineStyle){
-            var color = this.LineStyle.color.match(/.{2}/g);
-            styles[id] = {
-                lineWidth: this.LineStyle.width,
-                lineColor: color && "#" + color.splice(1).reverse().join('') || '',
-                lineOpacity: color && (parseInt(color[0],16) / 256) || ''
-            }
-        }
-        else if (this.IconStyle){
-            styles[id] = { icon : this.IconStyle.Icon.href }
-        }
-    });
-
-    var areas = {},
-        points = [];
-
-    $.each(data.Placemark, function(index, value){
-        if (this.Point){
-            points.push(this)
-        } else if (this.LineString){
-            var coords = this.LineString.coordinates.match(/([0-9.,]+)/g),
-                list = [],
-                s = styles[this.styleUrl] || {},
-                areaCoords = [],
-                middle = null
-            for (var c=null, x=0, y=0, i = 0, len = coords.length; i < len; i++){
-                c = coords[i].split(',');
-                x = parseFloat(c[1]);
-                y = parseFloat(c[0]);
-                areaCoords.push({x: x, y: y});
-                if (middle !== null){
-                    middle.x += x
-                    middle.y += y
-                } else middle = {x: x, y:y}
-                list.push(new google.maps.LatLng(x, y));
-            }
-            areas[this.name] = areaCoords
-            var flightPath = new google.maps.Polyline({
-                clickable: true,
-                path: list,
-                strokeColor: s.lineColor || '#FF0000',
-                strokeOpacity: s.lineOpacity || 1.0,
-                strokeWeight: s.lineWidth || 2,
-                map: map
-            });
-
-            var areaMarker = new google.maps.Marker({
-                position: new google.maps.LatLng(middle.x / list.length, middle.y / list.length),
-                title: this.name
-            });
-            google.maps.event.addListener(flightPath, 'click', function(e) {
-                infoWin.setContent("<h3>" + areaMarker.title + "</h3>")
-                infoWin.open(map,areaMarker)
-                map.panTo(areaMarker.position);
-            });
-            boundList = boundList.concat(list)
-        }
-    })
-    var pointCat = {}
-    $.each(points, function(idx, val){
-        var coords = this.Point.coordinates.split(','),
-            x = parseFloat(coords[1]),
-            y = parseFloat(coords[0]),
-            pos = new google.maps.LatLng(x, y),
-            marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                title: this.name
-            });
-        if (styles[this.styleUrl]){
-            marker.setIcon(styles[this.styleUrl].icon)
-        }
-        addEvent(marker, "<h3>" + this.name + "</h3>" + this.description)
-        var $marker = $('<li class="location"><span class="name">'+this.name+'</span><a href="#" class="zoom">[Nagyítás]</a></li>').data('marker', marker)
-        boundList.push(pos)
-        for (var areaName in areas){
-            if (isPointInPoly(areas[areaName], {x:x, y:y})){
-                if (!pointCat[areaName]) pointCat[areaName] = []
-                pointCat[areaName].push($marker)
-            }
-        }
-    })
-    areas = null
-
-    $.each(pointCat, function(name, arr){
-        var cat = $('<li><div class="group-title">'+name+'</div><ul class="places-list"></ul></li>').appendTo($mlist).find('ul');
-        for (var i = 0, l = arr.length; i < l; i++) arr[i].appendTo(cat);
-    });
-
-    var bounds = new google.maps.LatLngBounds (),
-        len = boundList.length;
-    while (len --) bounds.extend(boundList[len])
-
-    $fitBtn = $('<div class="fit-btn">Térkép igazítása</div>').on('click', function(){ map.fitBounds (bounds); }).click()
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push($fitBtn.get(0))
-
-}
-
-google.maps.event.addDomListener(window, 'load', function(){
-    var map = initializeMap();
-    //loadScript(KML_URL)
-    $.get('/api/counties', function(data){
-        _.forEach(data, function(county){
-            new google.maps.Polyline({
-                clickable: true,
-                path: _.map(county.coordinates, function(item) {
-                    return new google.maps.LatLng(item.lat, item.lng);
-                }),
-                strokeColor: county.style.color || '#FF0000',
-                strokeOpacity: county.style.opacity || 1.0,
-                strokeWeight: county.style.width || 2,
-                map: map
-            });
-            _.forEach(county.locations, function(marker){
-                var mMarker = new google.maps.Marker({
-                    position: new google.maps.LatLng(marker.coordinate.lat, marker.coordinate.lng),
-                    title: marker.name,
-                    map: map
+            function addEvent(target, marker, desc){
+                google.maps.event.addListener(target, 'click', function(e) {
+                    infoWin.setContent(desc)
+                    infoWin.open(map, marker)
+                    map.panTo(marker.position);
                 });
+            }
+
+
+            CountyService().then(function(result){
+                _.forEach(result.data, function(county){
+                    var countyBorder = new google.maps.Polyline({
+                        clickable: true,
+                        path: _.map(county.coordinates, function(item) {
+                            return new google.maps.LatLng(item.lat, item.lng);
+                        }),
+                        strokeColor: county.style.color || '#FF0000',
+                        strokeOpacity: county.style.opacity || 1.0,
+                        strokeWeight: county.style.width || 2,
+                        map: map
+                    });
+
+                    var countyCenter = _.reduce(county.coordinates, function(center, point){
+                        center.lat += point.lat;
+                        center.lng += point.lng;
+                        return center;
+                    }, {lat:0, lng:0});
+
+                    var countyMarker = new google.maps.Marker({
+                        position: new google.maps.LatLng(countyCenter.lat / county.coordinates.length,
+                                                         countyCenter.lng / county.coordinates.length),
+                        title: this.name
+                    });
+                    addEvent(countyBorder, countyMarker, '<h3>' + county.name  + '</h3>');
+
+                    _.forEach(county.locations, function(location){
+                        var marker = new google.maps.Marker({
+                            position: new google.maps.LatLng(location.coordinate.lat, location.coordinate.lng),
+                            title: location.name,
+                            map: map
+                        });
+                        addEvent(marker, marker, '<h3>' + location.name  + '</h3>' + location.description);
+                    });
+                });
+            }, function(err){
+                alert(err);
             });
-        });
-    })
-});
+        }
+    }
+}]);
+
+App.service('CountyService', ['$http', function($http){
+    return function(){
+        return $http.get('/api/counties');
+    }
+}]);
+
+App.controller('mainCtrl', ['$scope', 'CountyService', function($scope, CountyService){
+    CountyService().then(function(result){
+        $scope.counties = result;
+    }, function(err){
+        alert(err);
+    });
+}]);
