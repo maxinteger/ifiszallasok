af2b0d1cf4ff58f71e82cfa7853dce7851969731
Vadasz Laszlo <maxinteger@gmail.com>
2014-05-04 22:36:02 +0200

    basic admin page - edit locations II - map search

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,65 @@
  */
 Admin = angular.module('IfiszallasokAdmin', ['xeditable']);
 
+Admin.directive('mapPlaceSearch', function(){
+    return {
+        restrict: 'E',
+        require: 'ngModel',
+        replace: false,
+        template: '<div>' +
+            '<input type="text" placeholder="Keresés" class="form-control">' +
+            '<div class="map-canvas" style="height: 150px"></div>' +
+        '</div>',
+        link: function(scope, element, attrs, ngModel){
+            var marker = null,
+                map = new google.maps.Map(element.find('.map-canvas')[0], {
+                    zoom: 13,
+                    center: new google.maps.LatLng(47.0,19.0),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+
+            scope.data = {}
+
+            ngModel.$render = function modelRender(){
+                if (!ngModel.$viewValue || !ngModel.$viewValue.nodes){
+                    ngModel.$setViewValue({});
+                }
+                data = ngModel.$viewValue;
+            };
+
+            searchBox = new google.maps.places.SearchBox(element.find('input')[0]);
+            function serachCallbac() {
+                var place = searchBox.getPlaces()[0];
+                console.log(place);
+                scope.data = {
+                    lat: place.geometry.location.k,
+                    lng: place.geometry.location.A
+                };
+                if (marker){
+                    marker.setMap(null);
+                    delete marker;
+                }
+                marker = new google.maps.Marker({
+                    map: map,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+                map.panTo(marker.position);
+                scope.$apply(function(){
+                });
+                ngModel.$setViewValue(scope.data);
+            }
+
+            google.maps.event.addListener(searchBox, 'places_changed', serachCallbac);
+
+            scope.$on('$destroy', function(){
+                google.maps.event.removeListener(searchBox, 'places_changed');
+            });
+
+        }
+    }
+});
+
 Admin.controller('AdminMainCtrl', ['$scope', '$http', function($scope, $http){
     $scope.filter = {};
     $http.get('/api/counties').then(function(result){
